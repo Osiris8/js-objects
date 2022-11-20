2453e208363d89d59e4564ba2882cafdea14e82c
Osiris8 <miganosiris8@gmaaail.com>
2022-11-20 09:20:56 +0100

    Accessing Properties

---
+++ part2.js
@@ -0,0 +1,21 @@
+let spaceship = {
+  homePlanet: "Earth",
+  color: "silver",
+  "Fuel Type": "Turbo Fuel",
+  numCrew: 5,
+  flightPath: ["Venus", "Mars", "Saturn"],
+};
+
+// Write your code below
+
+let crewCount = spaceship.numCrew;
+
+let planetArray = spaceship.flightPath;
+
+console.log(crewCount);
+console.log(planetArray);
+
+/*
+5
+[ 'Venus', 'Mars', 'Saturn' ]
+*/

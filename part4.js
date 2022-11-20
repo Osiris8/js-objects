b4336fde1c261469ec6be05ce7b7fef1ffaf9ad6
Osiris8 <miganosiris8@gmaaail.com>
2022-11-20 09:24:25 +0100

    Property Assignment

---
+++ part4.js
@@ -0,0 +1,23 @@
+let spaceship = {
+  "Fuel Type": "Turbo Fuel",
+  homePlanet: "Earth",
+  color: "silver",
+  "Secret Mission": "Discover life outside of Earth.",
+};
+
+// Write your code below
+
+spaceship.color = "glorious gold";
+
+spaceship.numEngines = 9;
+
+delete spaceship["Secret Mission"];
+
+console.log(spaceship);
+
+/* Result
+{ 'Fuel Type': 'Turbo Fuel',
+  homePlanet: 'Earth',
+  color: 'glorious gold',
+  numEngines: 9 }
+  */

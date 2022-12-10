e7d0152ead62cb4e8d95dd99547d664504fca55d
Osiris8 <miganosiris8@gmaaail.com>
2022-12-10 07:22:48 +0100

    Destructured Assignment

---
+++ part18.js
@@ -0,0 +1,10 @@
+const vampire = {
+  name: "Dracula",
+  residence: "Transylvania",
+  preferences: {
+    day: "stay inside",
+    night: "satisfy appetite",
+  },
+};
+const residence = vampire.residence;
+console.log(residence); // Prints 'Transylvania
